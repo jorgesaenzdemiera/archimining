7600b66cdc0d42e4f67f9e9152e8e21d8134fd17
jorgesaenzdemiera <jorge.saenzdemiera@alumnos.upm.es>
2024-05-18 11:48:02 +0200

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,8 +1,4 @@
 import pandas as pd
-import os
-import matplotlib.pyplot as plt
-from PIL import Image
-import numpy as np
 import streamlit as st
 
 def show_images(names):
@@ -16,5 +12,5 @@ images_info = pd.read_csv(files_path + "images_info.csv", index_col = 0)
 
 images_ok = list(images_info.query("is_small == 0 and is_transparent == 0 and has_multiple_images == 0 and is_test == 0").index)
 
-st.write("# Architext")
+st.write("# Architextt")
 show_images(images_ok[:5])
